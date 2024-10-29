f7cda4c0741ba80fb1d960c2e6f37972e46ba5f4
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-10-29 19:34:31 +0200

    Create Contacts.js

---
+++ src/db/models/Contacts.js
@@ -0,0 +1,35 @@
+/* name - string, required
+phoneNumber - string, required
+email - string
+isFavourite - boolean, default false
+contactType - string, enum(’work’, ‘home’, ‘personal’), required, default ‘personal’
+ */
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  isFavourite: {
+    type: Boolean,
+    default: false,
+  },
+  contactType: {
+    type: String,
+    enum: ['work', 'home', 'personal'],
+    required: true,
+    default: 'personal',
+  },
+});
+
+const ContactCollection = model('contacts', contactSchema);
+
+export default ContactCollection;
