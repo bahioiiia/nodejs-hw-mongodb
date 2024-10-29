fef1f6a3e042276dca52b994c8dfe2fd4f20d4f6
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-10-29 20:56:49 +0200

    *

--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ import cors from 'cors';
 
 import { env } from "./utils/env.js";
 
+import * as contactServices from './services/contacts.js';
 
-
-export const startServer = () => {
+export const setupServer = () => {
     const app = express();
 
     app.use(cors());
@@ -20,75 +20,44 @@ export const startServer = () => {
     });
     // app.use(logger);
 
+    app.get('/contacts', async (req, res) => {
+    const data = await contactServices.getContacts();
+
+    res.json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data,
+    });
+    });
+
+    app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const data = await contactServices.getContactById(contactId);
+
+    if (!data) {
+        return res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+        });
+    }
+    res.status(200).json({
+        message: `Successfully found contact with id ${contactId}!`,
+        data,
+    });
+    });    
 	
+    app.use((req, res) => {
+    res.status(404).json({
+        message: 'Not found',
+    });
+    });
+
+    app.use((error, req, res, next) => {
+    res.status(500).json({
+        message: error.message,
+    });
+    });
+
 	const port = Number(env("PORT", 3000));
 	app.listen(port, () => console.log(`Server is running on ${port}`));
 };
-
-/* ------------------------------------------------------------------- */
-
-
-/* 
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
-
-import { env } from "./utils/env.js";
-
-import * as movieServices from "./services/movies.js";
-
-export const startServer = ()=> {
-    const app = express();
-
-    app.use(cors());
-    const logger = pino({
-        transport: {
-            target: "pino-pretty"
-        }
-    });
-    // app.use(logger);
-
-    app.get("/movies", async (req, res)=> {
-        const data = await movieServices.getMovies();
-
-        res.json({
-            status: 200,
-            message: "Successfully find movies",
-            data,
-        })
-    });
-
-    app.get("/movies/:id", async(req, res)=> {
-        const {id} = req.params;
-        const data = await movieServices.getMovieById(id);
-
-        if(!data) {
-            return res.status(404).json({
-                status: 404,
-                message: `Movie with id=${id} not found`,
-            })
-        }
-
-        res.json({
-            status: 200,
-            message: `Movie successfully find`,
-            data,
-        })
-;    })
-
-    app.use((req, res)=> {
-        res.status(404).json({
-            message: `${req.url} not found`
-        })
-    });
-
-    app.use((error, req, res, next)=> {
-        res.status(500).json({
-            message: error.message,
-        })
-    });
-
-    const port = Number(env("PORT", 3000));
-
-    app.listen(port, ()=> console.log(`Server running on ${port} PORT`));
-} */
