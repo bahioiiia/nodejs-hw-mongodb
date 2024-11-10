096e01dca57e5044df8edd8f1bfb4d02c1ef220a
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-11-10 16:09:26 +0200

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 export const errorHandler = (error, req, res, next) => {
-        const { status = 500, message = "Something went wrong" } = error;
-        res.status(500).json({
-            status,
-            message,
-        });
-}
+    const { status = 500, message = "Something went wrong" } = error;
+    res.status(status).json({
+        status,
+        message,
+    });
+};
