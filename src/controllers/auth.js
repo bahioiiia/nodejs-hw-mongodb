14c2cf8c5cf37b177a591a77b986450f862fd260
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-11-28 14:44:23 +0200

    5.4

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -61,9 +61,7 @@ export const logoutController = async (req, res) => {
     if (req.cookies.sessionId) {
         await authServices.logout(req.cookies.sessionId);
     }
-
     res.clearCookie("sessionId");
     res.clearCookie("refreshToken");
-
     res.status(204).send();
 };
